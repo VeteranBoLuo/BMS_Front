fc31765b1698142dffbe8d904d3ed31dfcb8964e
w30037884 <wenjunqiu@huawei.om>
2024-12-11 14:27:01 +0800

    颜色测试

--- src/components/BasicComponents/BInput/BInput.vue
+++ src/components/BasicComponents/BInput/BInput.vue
@@ -21,21 +21,16 @@
       v-else
       :id="id"
       class="b_input"
-      :class="inputTheme"
       :value="value"
-      :type="type"
       @input="handleInput"
       @enter="$emit('enter')"
       :style="{
         paddingLeft: hasPrefixSlot ? '30px' : '11px',
         paddingRight: hasSuffixSlot ? '30px' : '11px',
       }"
-      :maxlength="maxlength"
       :autocomplete="autocomplete"
       :placeholder="placeholder"
-      @change="$emit('change')"
       @focus="$emit('focus')"
-      @blur="$emit('blur')"
       @focusout="$emit('focusout')"
     />
     <div v-if="hasPrefixSlot" class="prefix-icon">
@@ -66,21 +61,9 @@
       type: String,
       default: 'off',
     },
-    height: {
-      type: String,
-      default: '32px',
-    },
-    theme: {
-      type: String,
-      default: '',
-    },
-    maxlength: {
-      type: [Number, String],
-      default: '',
-    },
   });
   const value = defineModel('value');
-  const emit = defineEmits(['input', 'enter', 'focus', 'focusout', 'blur', 'change']);
+  const emit = defineEmits(['input', 'enter', 'focus', 'focusout']);
 
   // 获取插槽内容
   const slots = useSlots();
@@ -99,13 +82,6 @@
     value.value = event.target.value;
     emit('input', event.target.value);
   }
-
-  const inputTheme = computed(() => {
-    if (props.theme) {
-      return 'input-' + props.theme;
-    }
-    return '';
-  });
 </script>
 <style lang="less" scoped>
   .input-container {
@@ -115,19 +91,18 @@
   .b_input {
     border: 1px solid #d9d9d9;
     border-radius: 6px;
-    padding: 0 11px;
-    height: v-bind(height);
+    padding: 4px 11px;
+    height: 32px;
     width: 100%;
     box-sizing: border-box;
     background-color: var(--bl-input-bg-color);
     color: var(--bl-input-color);
     &:focus {
       border: 1px solid var(--bl-input-border-h-color);
-      box-shadow: 0 0 0 1px rgba(92, 90, 86, 0.1);
+      box-shadow: 0 0 0 2px rgba(92, 90, 86, 0.1);
     }
     &:hover {
       border: 1px solid var(--bl-input-border-h-color);
-      transition: all 0.2s;
     }
     &:active {
       border: 1px solid var(--bl-input-border-h-color);
@@ -145,7 +120,7 @@
     font-family: '微软雅黑 Light', serif;
     &:focus {
       border: 1px solid var(--bl-input-border-h-color);
-      box-shadow: 0 0 0 1px rgba(92, 90, 86, 0.1);
+      box-shadow: 0 0 0 2px rgba(92, 90, 86, 0.1);
     }
     &:hover {
       border: 1px solid var(--bl-input-border-h-color);
@@ -160,7 +135,7 @@
     position: absolute;
     left: 10px;
     top: 0;
-    min-width: 16px;
+    width: 16px;
     height: 100%;
     display: grid;
     place-items: center;
@@ -169,7 +144,7 @@
     position: absolute;
     right: 10px;
     top: 0;
-    min-width: 16px;
+    width: 16px;
     height: 100%;
     display: grid;
     place-items: center;
@@ -179,16 +154,4 @@
     caret-color: var(--text-color);
     -webkit-text-fill-color: var(--text-color);
   }
-
-  .input-day {
-    background-color: #ffffff !important;
-    color: rgb(0, 0, 0) !important;
-  }
-  //@media (max-width: 600px) {
-  //  .b_input {
-  //    &:focus {
-  //      box-shadow: unset;
-  //    }
-  //  }
-  //}
 </style>
