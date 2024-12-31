90d66d6d25eab147b7779035039779c354534794
VeteranBoLuo <1902013368@qq.com>
2024-12-31 11:52:26 +0800

    建立websocket

--- src/App.vue
+++ src/App.vue
@@ -124,46 +124,6 @@
 
   window.fingerprint = fingerprint();
 
-  let socket;
-  let reconnectAttempts = 0;
-  const connect = () => {
-    let src = 'wss://139.9.83.16:3000';
-    if (window.location.protocol === 'http:') {
-      src = 'ws://127.0.0.1:3000';
-    }
-    // 建立WebSocket连接
-    socket = new WebSocket(src);
 
-    // 当连接打开时触发
-    socket.onopen = () => {
-      console.log('WebSocket 已连接:');
-      reconnectAttempts = 0; // 重置重连尝试次数
-    };
-
-    // 当接收到消息时触发
-    socket.onmessage = (event) => {
-      console.log('收到消息:', event.data);
-    };
-
-    // 当连接关闭时触发
-    socket.onclose = (event) => {
-      console.log('WebSocket 连接已关闭:', event);
-      // 延迟重连
-      const reconnectDelay = Math.min(1000 * Math.pow(2, reconnectAttempts), 30000); // 最大30秒
-      setTimeout(() => {
-        reconnectAttempts++;
-        console.log(`尝试第${reconnectAttempts}次重新连接...`);
-        connect();
-      }, reconnectDelay);
-    };
-
-    // 当连接出错时触发
-    socket.onerror = (error) => {
-      console.error('WebSocket 错误:', error);
-    };
-  };
-
-  // 初始连接
-  connect();
 </script>
 <style></style>
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,11 @@ import '@/assets/css/index.less';
 import { Icon } from '@iconify/vue';
 import globalDirect from '@/config/globalDirect';
 import { createPinia } from 'pinia';
-
+import { connect } from '@/http/websocket';
 // 创建vue实例
 const app = createApp(App);
 const pinia = createPinia();
+connect();
 app.use(pinia);
 app.use(router);
 app.component('Icon', Icon);
