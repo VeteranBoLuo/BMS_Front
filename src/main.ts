1b4ef2c93fe7a93c27dbe18d847b0132fd1330bd
VeteranBoLuo <1902013368@qq.com>
2024-12-31 17:15:20 +0800

    建立websocket

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,11 @@ import '@/assets/css/index.less';
 import { Icon } from '@iconify/vue';
 import globalDirect from '@/config/globalDirect';
 import { createPinia } from 'pinia';
-import { connect } from '@/http/websocket';
+// import { connect } from '@/http/websocket';
 // 创建vue实例
 const app = createApp(App);
 const pinia = createPinia();
-connect();
+// connect();
 app.use(pinia);
 app.use(router);
 app.component('Icon', Icon);
