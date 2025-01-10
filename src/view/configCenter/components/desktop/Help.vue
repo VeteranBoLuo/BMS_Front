5fa92adf4229428c28e1dc77fd12a7f804476ac9
VeteranBoLuo <1902013368@qq.com>
2025-01-10 16:48:37 +0800

    帮助中心优化1=

--- src/view/configCenter/components/desktop/Help.vue
+++ src/view/configCenter/components/desktop/Help.vue
@@ -1,12 +1,5 @@
 <template>
   <div class="help-container">
-    <!--    <b-button-->
-    <!--      style="position: absolute; right: 20px"-->
-    <!--      :style="{ left: bookmark.isPhone ? '' : '187px' }"-->
-    <!--      @click="handleToBack"-->
-    <!--      v-click-log="{ module: '帮助中心', operation: `返回` }"-->
-    <!--      >返回</b-button-->
-    <!--    >-->
     <b-input
       v-model:value="searchValue"
       placeholder="请搜索问题"
@@ -69,28 +62,32 @@
 </template>
 
 <script lang="ts" setup>
-  import { computed, nextTick, onMounted, onUnmounted, ref, watch } from 'vue';
+  import { computed, nextTick, onMounted, onUnmounted, ref } from 'vue';
   import BList from '@/components/BasicComponents/BList/BList.vue';
   import icon from '@/config/icon.ts';
   import SvgIcon from '@/components/SvgIcon/src/SvgIcon.vue';
   import { bookmarkStore, tourStore } from '@/store';
   import BInput from '@/components/BasicComponents/BInput/BInput.vue';
-  import BButton from '@/components/BasicComponents/BButton/BButton.vue';
-  import BSpace from '@/components/BasicComponents/BSpace/BSpace.vue';
 
   import tagAndBookmark from '@/assets/img/help/tag_bookmark_relation.jpg';
-  import addTagAndBookmark from '@/assets/img/help/add_data.jpg';
-  import tagEdit from '@/assets/video/tag_edit.webm';
-  import bookmarkEdit from '@/assets/video/bookmark_edit.webm';
-  import tagMg from '@/assets/img/help/tagMg.jpg';
-  import bookmarkMg from '@/assets/img/help/bookmarkMg.jpg';
+  import tagMg from '@/assets/img/help/tag_mg.jpg';
+  import tagAddBtn from '@/assets/img/help/tag_addbtn.jpg';
+  import tagAddPage from '@/assets/img/help/tag_addPage.jpg';
+  import tagTable from '@/assets/img/help/tag_table.jpg';
+  import tagTree from '@/assets/img/help/tag_tree.jpg';
+
+  import bookmarkMg from '@/assets/img/help/bookmark_mg.jpg';
+  import bookmarkAddPage from '@/assets/img/help/bookmark_addPage.jpg';
+  import bookmarkMainPage from '@/assets/img/help/bookmark_mainPage.jpg';
+  import bookmarkTable from '@/assets/img/help/bookmark_table.jpg';
+
   import opinionMg from '@/assets/img/help/opinionMg.png';
 
   import 'viewerjs/dist/viewer.css'; //样式文件不要忘了
   import router from '@/router';
 
   const bookmark = bookmarkStore();
-  const checkId = ref('');
+  const checkId = ref('标签管理');
   function logItem(item) {
     checkId.value = item.id;
     nextTick(() => {
@@ -116,153 +113,117 @@
       router.push('/home');
     }
   }
-  const listOptions = ref([
-    {
-      id: '书签与标签',
-      title: '书签与标签',
-      content: `<div class="tag-explanation">
-    <h2>理解标签与书签</h2>
+    const listOptions = ref([
+      {
+        id: '书签与标签',
+        title: '书签与标签',
+        content: `<div class="tag-explanation" >
+      <h2>理解标签与书签</h2>
 
+        <div class="bookmark-definition">
+        <h3>什么是标签？</h3>
+       <p>标签是用于描述内容、分类或属性的关键词，它帮助我们组织和分类信息。在本网站中，标签用于对书签进行分类，使得查找和浏览更加高效。</p>
+      </div>
       <div class="bookmark-definition">
-      <h3>什么是标签？</h3>
-     <p>标签是用于描述内容、分类或属性的关键词，它帮助我们组织和分类信息。在本网站中，标签用于对书签进行分类，使得查找和浏览更加高效。</p>
-    </div>
-    <div class="bookmark-definition">
-      <h3>什么是书签？</h3>
-      <p>书签是保存网站链接以便快速访问的一种方式。你可以将喜欢的网页或资源添加到书签中，以便日后快速检索。</p>
-    </div>
+        <h3>什么是书签？</h3>
+        <p>书签是保存网站链接以便快速访问的一种方式。你可以将喜欢的网页或资源添加到书签中，以便日后快速检索。</p>
+      </div>
 
-    <h3>标签与书签的关系</h3>
-    <p>一个标签下可以有多个书签。例如，"文档"标签可能包含各种文档网站的书签，菜鸟教程、Vue官方网站等。</p>
-    <p>同样，一个书签也可以属于多个标签。例如，Vue的官方网站可能同时属于"Vue"和"文档"这两个标签，因为它既与Vue相关，又是一个文档网站。</p>
+      <h3>标签与书签的关系</h3>
+      <p>一个标签下可以有多个书签。例如，"文档"标签可能包含各种文档网站的书签，菜鸟教程、Vue官方网站等。</p>
+      <p>同样，一个书签也可以属于多个标签。例如，Vue的官方网站可能同时属于"Vue"和"文档"这两个标签，因为它既与Vue相关，又是一个文档网站。</p>
 
-    <div class="bookmark-example">
-      <h3>关系示例</h3>
-      <img src="${tagAndBookmark}"  alt="书签示例" class="bookmark-image"   />
-      <p style="font-size: 12px">
-      <i>书签标签关系图</i>
-  </p>
-    </div>
-  </div>`,
-    },
-    {
-      id: '数据管理',
-      title: '数据管理',
-      content: `<div class="tag-explanation">
-    <div class="bookmark-example">
-      <img src="${addTagAndBookmark}" alt="书签示例" class="bookmark-image">
-      <p style="font-size: 12px">
-      <i>快速新增示意图</i>
-  </p>
-    </div>
-    <div class="bookmark-example">
-      <img style="width: 80%" src="${tagMg}" alt="标签管理示例" class="bookmark-image">
-      <p style="font-size: 12px">
-      <i>标签管理页面</i>
-  </p>
-    </div>
-    <div class="bookmark-example">
-      <img style="width: 80%" src="${bookmarkMg}" alt="书签管理" class="bookmark-image">
-      <p style="font-size: 12px">
-      <i>书签管理页面</i>
-  </p>
-    </div>
-  </div>`,
-    },
-    {
-      id: '标签管理',
-      title: '标签管理',
-      content: `<div class="tag-explanation">
-    <div class="bookmark-example">
-     <video width="85%" controls>
-    <source src="${tagEdit}" />
-  </video>
-      <p style="font-size: 12px">
-      <i>标签管理演示视频</i>
-  </p>
-    </div>
-    <p>1、标签图标支持手动填写和文件上传</p>
-    <p>2、手动填写时，图标网站强烈推荐 <a target="_blank" href="https://icon-sets.iconify.design/"><b>Iconify</b></a>，视频演示中获取图标的网站就是Iconify。
-    Iconify提供了大量的开源图标集，适用于各种项目。这些图标集涵盖了多种风格和主题，如Material Design、UI设计、编程标志、表情符号等。<br/>
-    网站支持筛选功能，便于快速找到所需图标。此外，Iconify提供的图标具有多种尺寸和格式，灵活适应不同的设计需求。最重要的是，这些图标都是开源的，
-    使用起来非常方便。</p>
-  <p>3、设置标签图标时，可支持的格式有svg、base64、图标在线地和iconfy的vue组件等，直接复制即可，例如：</p>
-   <ul style="text-align: left">
-      <li>svg：<div class="svg-code">&lt;svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24">
-  \t&lt;rect width="24" height="24" fill="none" />
-  \t&lt;path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M14.5 2v17.5c0 1.4-1.1 2.5-2.5 2.5s-2.5-1.1-2.5-2.5V2m-1 0h7m-1 14h-5" />
-  &lt;/svg></div>
-  </li>
-      <li>base64：<div class="svg-code">data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxZW0iIGhlaWdo
-      dD0iMWVtIiB2aWV3Qm94PSIwIDAgMjQgMjQiPgoJPHJlY3Qgd2lkdGg9IjI0IiBoZWlnaHQ9IjI0IiBmaWxsPSJub25lIiAvPgoJPHBhdGggZmlsbD0i
-      bm9uZSIgc3Ryb2tlPSJjdXJyZW50Q29sb3IiIHN0cm9rZS1saW5lY2FwPSJyb3VuZCIgc3Ryb2tlLWxpbmVqb2luPSJyb3VuZCIgc3Ryb2tlLXdpZHRo
-      PSIyIiBkPSJNMTQuNSAydjE3LjVjMCAxLjQtMS4xIDIuNS0yLjUgMi41cy0yLjUtMS4xLTIuNS0yLjVWMm0tMSAwaDdtLTEgMTRoLTUiIC8+Cjwvc3Z
-      nPg==</div</li>
-      <li>在线地址：<div class="svg-code">https://img2.baidu.com/it/u=399286100,2135569916&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=625<div/></li>
-      <li>iconfy的vue组件：<div class="svg-code">&lt;Icon icon="lucide:test-tube" /></div</li>
-    </ul>
-  </div>`,
-    },
-    {
-      id: '书签管理',
-      title: '书签管理',
-      content: `<div class="tag-explanation">
-    <div class="bookmark-example">
+      <div class="bookmark-example">
+        <h3>关系示例</h3>
+        <img src="${tagAndBookmark}"  alt="书签示例" class="bookmark-image"   />
+        <p style="font-size: 12px">
+        <i>书签标签关系图</i>
+    </p>
+      </div>
+    </div>`,
+      },
+      {
+        id: '标签管理',
+        title: '标签管理',
+        content: `<div style="width: 80%;margin: auto">
+      <div class="bookmark-example">
+        <p style="font-size: 12px">
+        <p style="text-align: left">1、点击标签管理进入标签管理页面</p>
+        <img src="${tagMg}"  alt="标签示例" class="bookmark-image"   width="80%" style="border: 1px solid;"/>
+        <p><i style="text-align: center;font-size: 12px">标签管理</i></p>
+         <p style="text-align: left">2、点击新增进入添加标签页面</p>
+        <img src="${tagAddBtn}"  alt="标签示例" class="bookmark-image"   width="80%" style="border: 1px solid;"/>
+         <p style="text-align: left">3、标签图标可通过本地上传或粘贴图标的svg代码、base64代码等两种方式设置</p>
+        <img src="${tagAddPage}"  alt="标签示例" class="bookmark-image"   width="80%" style="border: 1px solid;"/>
+         <p style="text-align: left">4、新增成功后可在标签管理页面和首页查看</p>
+        <img src="${tagTable}"  alt="标签示例" class="bookmark-image"   width="80%" style="border: 1px solid;"/>
+         <img src="${tagTree}"  alt="标签示例" class="bookmark-image"   width="80%" style="border: 1px solid;"/>
+    </p>
+        <i style="text-align: center">标签管理示例</i>
+      </div>
+    </div>`,
+      },
+      {
+        id: '书签管理',
+        title: '书签管理',
+        content: `<div class="tag-explanation" style="width: 80%;margin: auto">
+         <div class="bookmark-example">
+        <p style="font-size: 12px">
+         <p style="text-align: left">1、点击书签管理进入书签管理页面，或者直接右键标签快速创建与此标签相关的书签</p>
+        <img src="${bookmarkMg}"  alt="书签示例" class="bookmark-image"   width="80%" style="border: 1px solid;"/>
+          <p style="text-align: left">2、书签无需上传图标，系统会自动根据网站地址生成</p>
+        <img src="${bookmarkAddPage}"  alt="书签示例" class="bookmark-image"   width="80%" style="border: 1px solid;"/>
+          <p style="text-align: left">3、新增成功后可在书签管理页面和首页查看</p>
+        <img src="${bookmarkTable}"  alt="书签示例" class="bookmark-image"   width="80%" style="border: 1px solid;"/>
+        <img src="${bookmarkMainPage}"  alt="书签示例" class="bookmark-image"   width="80%" style="border: 1px solid;"/>
+    </p>
+        <i>书签管理示例</i>
+    </div>`,
+      },
+      {
+        id: '意见反馈',
+        title: '意见反馈',
+        content: `<div class="tag-explanation" >
        <div class="bookmark-example">
-     <video width="85%" controls>
-    <source src="${bookmarkEdit}" />
-  </video>
-      <p style="font-size: 12px">
-      <i>书签管理演示视频</i>
-  </p>
-    </div>
-  <p>系统会根据书签的网站地址，自动获取网站图标</p>
-  </div>`,
-    },
-    {
-      id: '意见反馈',
-      title: '意见反馈',
-      content: `<div class="tag-explanation" >
-     <div class="bookmark-example">
-      <img style="width: 80%" src="${opinionMg}" alt="标签管理示例" class="bookmark-image">
-      <p style="font-size: 12px">
-      <i>意见反馈页面</i>
-  </p>
-    </div>
-  <p><li>模块宗旨</li><span style="font-size: 14px">意见反馈模块旨在搭建一个用户与项目开发人员之间沟通的桥梁，便于您及时向我反映在使用过程中遇到的问题、提出的建议以及期望的功能，帮助我不断优化产品，为您提供更优质的服务。</span>
-<br/>
-<li>模块功能</li>
-<span style="font-size: 14px">问题反馈：您可以在此提交在使用项目过程中遇到的问题，如功能异常、操作不便等，我将尽快为您解决。<br/>
-建议征集：欢迎您提出宝贵建议，包括功能优化、界面美化、操作简化等方面，我会认真倾听并积极采纳。<br/>
-点赞与鼓励：如果您对我的项目表示满意，也可以在此给予我肯定和鼓励，激励我继续努力。<br/>
-实时跟进：提交反馈后，您可以实时查看反馈处理进度，了解解决结果。</span>
+        <img style="width: 80%" src="${opinionMg}" alt="标签管理示例" class="bookmark-image">
+        <p style="font-size: 12px">
+        <i>意见反馈页面</i>
+    </p>
+      </div>
+    <p><li>模块宗旨</li><span style="font-size: 14px">意见反馈模块旨在搭建一个用户与项目开发人员之间沟通的桥梁，便于您及时向我反映在使用过程中遇到的问题、提出的建议以及期望的功能，帮助我不断优化产品，为您提供更优质的服务。</span>
+  <br/>
+  <li>模块功能</li>
+  <span style="font-size: 14px">问题反馈：您可以在此提交在使用项目过程中遇到的问题，如功能异常、操作不便等，我将尽快为您解决。<br/>
+  建议征集：欢迎您提出宝贵建议，包括功能优化、界面美化、操作简化等方面，我会认真倾听并积极采纳。<br/>
+  点赞与鼓励：如果您对我的项目表示满意，也可以在此给予我肯定和鼓励，激励我继续努力。<br/>
+  实时跟进：提交反馈后，您可以实时查看反馈处理进度，了解解决结果。</span>
 
-<br/>
+  <br/>
 
-<li>使用指南</li>
-<span style="font-size: 14px">进入意见反馈模块：项目右上角头像下拉框中找到“意见反馈”入口，点击进入。
-填写反馈内容：根据提示，详细描述您遇到的问题或建议，以便我更快地为您解决问题。<br/>
-提交反馈：确认无误后，点击“提交”按钮，您的反馈将实时发送至项目团队。<br/>
-查看反馈进度：在“我的反馈”页面，您可以查看已提交反馈的处理进度及回复。</span>
+  <li>使用指南</li>
+  <span style="font-size: 14px">进入意见反馈模块：项目右上角头像下拉框中找到“意见反馈”入口，点击进入。
+  填写反馈内容：根据提示，详细描述您遇到的问题或建议，以便我更快地为您解决问题。<br/>
+  提交反馈：确认无误后，点击“提交”按钮，您的反馈将实时发送至项目团队。<br/>
+  查看反馈进度：在“我的反馈”页面，您可以查看已提交反馈的处理进度及回复。</span>
 
-<br/>
+  <br/>
 
-<li>温馨提示</li>
-<span style="font-size: 14px">请确保反馈内容真实、客观、具体，以便我更好地为您解决问题。
-为保护您的隐私，请勿在反馈内容中透露个人信息。
-我会尽快处理您的反馈，请您耐心等待。</span>
-<br/>
-感谢您对项目的支持与关注，您的意见是我不断进步的动力！让我们共同努力，打造更优质的产品和服务。</p>
-  </div>`,
-    },
-  ]);
-  const content = computed(() => {
-    const item = listOptions.value.find((data) => data.id === checkId.value);
-    if (item) {
-      return item.content || '';
-    }
-    return helpInfo;
-  });
+  <li>温馨提示</li>
+  <span style="font-size: 14px">请确保反馈内容真实、客观、具体，以便我更好地为您解决问题。
+  为保护您的隐私，请勿在反馈内容中透露个人信息。
+  我会尽快处理您的反馈，请您耐心等待。</span>
+  <br/>
+  感谢您对项目的支持与关注，您的意见是我不断进步的动力！让我们共同努力，打造更优质的产品和服务。</p>
+    </div>`,
+      },
+    ]);
+    const content = computed(() => {
+      const item = listOptions.value.find((data) => data.id === checkId.value);
+      if (item) {
+        return item.content || '';
+      }
+      return helpInfo;
+    });
 
   const helpInfo = `<div class="help-document-intro">
     <h2>欢迎来到帮助中心</h2>
