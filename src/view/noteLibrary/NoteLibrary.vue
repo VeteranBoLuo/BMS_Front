cd15c7c092a19bd8aec189f9a6f998feb5fd1f26
w30037884 <wenjunqiu@huawei.com>
2025-02-13 16:38:03 +0800

    笔记库主题优化

--- src/view/noteLibrary/NoteLibrary.vue
+++ src/view/noteLibrary/NoteLibrary.vue
@@ -129,7 +129,7 @@
   .note-library-body {
     width: 100%;
     height: 100%;
-    min-height: 400px;
+    min-height: calc(100vh - 100px);
     padding: 20px;
     display: grid;
     grid-template-columns: repeat(auto-fill, minmax(240px, 1fr));
