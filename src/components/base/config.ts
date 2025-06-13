001d7b07f84f24215b31630c319af791f9f36f64
w30037884 <wenjunqiu@huawei.com>
2025-06-13 09:30:57 +0800

    --

--- src/components/base/config.ts
+++ src/components/base/config.ts
@@ -1,58 +1,65 @@
-import _ from 'lodash-es';
+import { isArray, isEmpty, mergeWith } from 'lodash-es';
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
+import { ConfigMap } from '@/components/base/types';
 
 function customizer(obj: any, src: any) {
-  if (_.isArray(obj)) {
+  if (isArray(obj)) {
     return src;
   }
-  if (_.isEmpty(src)) {
+  if (isEmpty(src)) {
     return src;
   }
 }
 
-export function defineComponentConfig(config_id: string) {
+export function defineComponentConfig<T extends keyof ConfigMap>(config_id: string) {
   return defineStore(config_id, () => {
     const ref_config = ref({
-      settings: {} as any,
+      settings: {} as ConfigMap[T]['settings'],
       methods: {} as any,
       events: {} as any,
       styles: {} as any,
       classes: {} as any,
     });
 
-    let default_config = {};
-    let config = {};
-    function setConfig(config_new: any) {
-      config = _.mergeWith(config, config_new, customizer);
+    let default_config: Partial<ConfigMap[T]> = {};
+    let config: Partial<ConfigMap[T]> = {};
+
+    function setConfig(config_new: Partial<ConfigMap[T]>) {
+      config = mergeWith(config, config_new, customizer);
       mergeConfig();
     }
 
-    function setDefaultConfig(default_config_new: any) {
+    function setDefaultConfig(default_config_new: Partial<ConfigMap[T]>) {
       default_config = default_config_new;
       mergeConfig();
     }
 
     function mergeConfig() {
-      const config_final = _.mergeWith(default_config, config, customizer);
-      _.mergeWith(ref_config.value, config_final, customizer);
+      const config_final = mergeWith(default_config, config, customizer);
+      mergeWith(ref_config.value, config_final, customizer);
     }
 
     function useConfig() {
       return ref_config;
     }
+
     function useSettings() {
       return ref(ref_config.value.settings);
     }
+
     function useMethods() {
       return ref(ref_config.value.methods);
     }
+
     function useEvents() {
       return ref(ref_config.value.events);
     }
+
     function useStyles() {
       return ref(ref_config.value.styles);
     }
+
     function useClasses() {
       return ref(ref_config.value.classes);
     }
@@ -70,8 +77,8 @@ export function defineComponentConfig(config_id: string) {
   });
 }
 
-export function useComponentConfigStoreById(config_id: string) {
-  const useConfigStore = defineComponentConfig(config_id);
+export function useComponentConfigStoreById<T extends keyof ConfigMap>(config_id: string) {
+  const useConfigStore = defineComponentConfig<T>(config_id);
   return useConfigStore();
 }
 
