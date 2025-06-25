a0183a13534069a9e6af71eb02d4a42c91c193b2
w30037884 <wenjunqiu@huawei.com>
2025-06-25 18:09:05 +0800

    1、主题切换组件重构，新增支持跟随系统变化 2、书签和标签的新增和管理入口统一放置在顶部导航栏右侧区域 3、首页书签列表查询动画效果优化

---
+++ src/components/base/BasicComponents/BMenu.vue
@@ -0,0 +1,25 @@
+<template>
+  <a-dropdown>
+    <slot />
+    <template #overlay>
+      <a-menu>
+        <a-menu-item v-for="item in menuOptions" :key="item.label" @click="item.function">
+          <div class="flex-align-center-gap">
+            <svg-icon v-if="item.icon" :src="item.icon" />
+            <span>{{ item.label }}</span>
+          </div>
+        </a-menu-item>
+      </a-menu>
+    </template>
+  </a-dropdown>
+</template>
+
+<script lang="ts" setup>
+  import SvgIcon from '@/components/base/SvgIcon/src/SvgIcon.vue';
+
+  const props = defineProps<{
+    menuOptions: { label: string; icon?: string; function?: () => void }[];
+  }>();
+</script>
+
+<style lang="less" scoped></style>
