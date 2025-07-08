5f4fc5256b767b91209cc19ac9d04986c76efa06
w30037884 <wenjunqiu@huawei.com>
2025-07-08 17:17:55 +0800

    ---

--- src/view/auth/callback/GithubCallBack.vue
+++ src/view/auth/callback/GithubCallBack.vue
@@ -10,13 +10,6 @@
 
   onMounted(async () => {
     const code = route.query.code;
-    const state = route.query.state;
-
-    // 验证 state 防止 CSRF
-    if (!state || state !== localStorage.getItem('github_state')) {
-      router.push('/login');
-      return;
-    }
 
     // 发送 code 给后端换取 Token
     try {
@@ -24,8 +17,8 @@
       const { user_info } = res.data;
       user.setUserInfo(user_info);
       // 存储用户信息（示例）
-      localStorage.setItem('userId',user_info.id);
-      router.push('/home');
+      localStorage.setItem('userId', user_info.id);
+      location.reload();
     } catch (error) {
       console.error('登录失败', error);
     }
